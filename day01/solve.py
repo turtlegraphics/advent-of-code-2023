6eb5008818bb8607275314fbced29d986559d3b8
Bryan Clair <bryan@slu.edu>
2023-11-30 23:23:01 -0600

    Initial setup

---
+++ day01/solve.py
@@ -0,0 +1,24 @@
+#
+# Advent of Code 2022
+# Bryan Clair
+#
+# Day --
+#
+import sys
+sys.path.append("..")
+import aocutils
+
+args = aocutils.parse_args()
+
+inputlines = [x.strip() for x in open(args.file).readlines()]
+
+# import re
+# parser = re.compile(r"name:\s*(\w+)\s*val:\s*(\d+)") # or whatever
+# name, val = parser.match(line).groups()
+# val = int(val)
+
+for line in inputlines:
+    print(line)
+
+
+    
