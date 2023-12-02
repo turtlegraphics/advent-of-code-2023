eae462c4d8334e7857cf0bcffe6afe00d75d02e4
Bryan Clair <bryan@slu.edu>
2023-12-01 23:18:07 -0600

    I felt fast tonight, code is clean too.

--- day02/solve.py
+++ day02/solve.py
@@ -19,21 +19,32 @@ bagmax = {
     }
 
 part1 = 0
+part2 = 0
 for line in inputlines:
     game,colors = line.split(':')
     game_id = int(game.split(' ')[1])
     # print('Game',game_id)
     game_ok = True
+    minset = {
+        'red':0,
+        'green':0,
+        'blue':0
+    }
     sets = colors.split(';')
     for s in sets:
         # print('set:',s)
         for c in s.split(','):
             num, col = c.strip().split(' ')
-            if int(num) > bagmax[col]:
+            num = int(num)
+            if num > bagmax[col]:
                 game_ok = False
+            minset[col] = max(minset[col],num)
+            
     if game_ok:
         part1 += game_id
+    part2 += minset['red']*minset['green']*minset['blue']
 
 print('part1:',part1)
+print('part2:',part2)
             
     
