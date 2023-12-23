3730b7c7aed24293bccd57c0eebfab4c1aa5e3ab
Bryan Clair <bryan@slu.edu>
2023-12-23 00:36:59 -0800

    networkx is useful, so commenting.

--- day23/solve2.py
+++ day23/solve2.py
@@ -26,42 +26,68 @@ for t in trails:
 start = (1,trails.height()-1)
 finish = (trails.width()-2,0)
 
-def dfs(G, loc):
-    loc = tuple(loc)
-    trails[loc] = 'o'
-    G.add_node(loc)
-    for n in trails.neighbors(loc):
-        if trails[n] == '#':
-            continue
-        if trails[n] == 'o':
-            G.add_edge(loc,tuple(n),weight=1)
-        if trails[n] == '.':
-            dfs(G, n)
+# The following code converts the Grid to a networkx Graph object
+# first run dfs to build a Graph where each node is a Grid coordinate
+# then go through and eliminate all degree 2 nodes
+# keep track of lengths as the 'weight' attribute
+# when two paths connect the same two nodes, keep the longer one
 
-G = nx.Graph()
-dfs(G, start)
-for node, degree in dict(G.degree()).items():
-    if degree == 2:
-        nbrs = []
-        weight = 0
-        for n,v,data in G.edges(node, data=True):
-            assert(n==node)
-            nbrs.append(v)
-            weight += data['weight']
-        if G.has_edge(nbrs[0],nbrs[1]):
-            oldweight = G.get_edge_data(nbrs[0],nbrs[1])['weight']
-            weight = max(oldweight,weight)
-        G.add_edge(nbrs[0],nbrs[1],weight=weight)
-        G.remove_node(node)
+def dfs(maze, G, loc):
+    """DFS through Grid maze, creating nodes and edges in a Graph G."""
+    loc = tuple(loc)
+    maze[loc] = 'o'
+    G.add_node(loc)
+    for n in maze.neighbors(loc):
+        if maze[n] == '#':
+            continue
+        if maze[n] == 'o':
+            G.add_edge(loc,tuple(n),weight=1)
+        if maze[n] == '.':
+            dfs(maze, G, n)
+
+def condense(G):
+    """
+    Take a Graph G and remove degree 2 nodes, keeping connections.
+    Calculates lengths.  When two paths connect the same two nodes,
+    it keeps the longer one.
+    """
+    for node, degree in dict(G.degree()).items():
+        if degree == 2:
+            nbrs = []
+            weight = 0
+            for n,v,data in G.edges(node, data=True):
+                assert(n==node)
+                nbrs.append(v)
+                weight += data['weight']
+            if G.has_edge(nbrs[0],nbrs[1]):
+                # this is not the first path connecting these nodes
+                # for this problem, we want the longer of the two
+                # possible paths.  definitely watch out for this if
+                # you re-use this code
+                oldweight = G.get_edge_data(nbrs[0],nbrs[1])['weight']
+                weight = max(oldweight,weight)
+            G.add_edge(nbrs[0],nbrs[1],weight=weight)
+            G.remove_node(node)
+
+def Grid_to_Graph(maze):
+    """Convert from a Grid object to a networkx Graph."""
+    from copy import deepcopy
+    G = nx.Graph()
+    dfs(deepcopy(trails), G, start)
+    condense(G)
+    return G
 
 def path_length(p):
+    """why do I need this? because networkx path length function seems to be
+    incompatible with its own paths."""
     l = 0
     for e in p:
         l += G.get_edge_data(*e)['weight']
     return l
 
-print('part 2: showing best so far (total runtime < 3 minutes)')
+G = Grid_to_Graph(trails)
 
+print('part 2: showing best so far (total runtime < 3 minutes)')
 best = 0
 for path in nx.all_simple_edge_paths(G,start,finish):
     l = path_length(path)
